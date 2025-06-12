389f552d57ac8448e0cca9942ad4c9f8527409e4
alfredosicre <alfredo.sicre@gmail.com>
2025-06-12 13:46:01 +0200

    11junio202501

--- src/App.java
+++ src/App.java
@@ -1,10 +1,20 @@
-import com.objetos.Prueba;
+import com.objetos.Persona;
 
 public class App {
     public static void main(String[] args) throws Exception {
         
-        System.out.println("Hello, World!");
-        Prueba.saludar();
+        System.out.println("Probando la clase Persona");
+
+        // instanciamos el objeto Persona
+        Persona person = new Persona();
+        
+        // ya podemos utilizar a Persona
+        person.nombre = "Lucas";
+        person.apellidos = "Lopez";
+        person.edad = 29;
+
+        System.out.println(person.nombre + " " + person.apellidos + " " + " Edad: " + person.edad);
+   
         
     }
 }
---
+++ src/com/objetos/Persona.java
@@ -0,0 +1,9 @@
+package com.objetos;
+
+public class Persona {
+
+    public String nombre;
+    public String apellidos;
+    public int edad;
+    
+}
