617c3eadf9b1730f62768789b10ed8be07ceacd1
alfredosicre <alfredo.sicre@gmail.com>
2025-06-12 13:21:52 +0200

    first commit

---
+++ src/com/objetos/Prueba.java
@@ -0,0 +1,12 @@
+package com.objetos;
+
+public class Prueba {
+
+    // esta clase no se va a ejecutar
+    // creamos un metodo para saludar
+    
+    public static void saludar() {
+        System.out.println("Saludando desde Prueba");
+    }
+    
+}
