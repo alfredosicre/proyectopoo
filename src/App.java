e3b25e32c5652c38e4384c96cb7ed7348ccea93a
alfredosicre <alfredo.sicre@gmail.com>
2025-06-13 13:45:17 +0200

    13junio202501

--- src/App.java
+++ src/App.java
@@ -1,20 +1,47 @@
 import com.objetos.Persona;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         
         System.out.println("Probando la clase Persona");
 
-        // instanciamos el objeto Persona
-        Persona person = new Persona();
-        
-        // ya podemos utilizar a Persona
-        person.nombre = "Lucas";
-        person.apellidos = "Lopez";
-        person.edad = 29;
+        try{
 
-        System.out.println(person.nombre + " " + person.apellidos + " " + " Edad: " + person.edad);
-   
+        // instanciamos el objeto Persona
+        Persona person = new Persona(); // llamamos a la clase Persona.java
         
+        // ya podemos utilizar a Persona.java
+        person.setNombre("Lucas");
+        person.setApellidos("Lopez");
+        person.setEdad(45);
+        person.setGenero(Persona.tipoGenero.MASCULINO);
+        person.setDni(2211537);
+    
+
+        System.out.println(person.getNombre() + " " + person.getApellidos() + " " + " Edad: " + person.getEdad() + " sexo: "+ person.getGenero());
+        System.out.println("Nombre completo: " + person.getNombreCompleto());
+        System.out.println("Nombre completo: " + person.getNombreCompletoMayusculas());
+        System.out.println("Nombre completo: " + person.getNombreCompletoAlreves());
+        System.out.println("La letra de el DNI:  " + person.getDni() +" es: " + person.getLetraDni());    
+
+
+        }catch (Exception e){
+
+            //nunca dejaremos un catch negativo
+            System.out.println("Error: " + e.getMessage());
+        }
+           
     }
-}
+
+} 
+
+// pasos a ejecutar con una base de datos
+// try{
+//  1- nos conectamos a la bbdd
+//  2- leemos registros
+//  3- cerramos la conexion
+//}catch (Exception e){
+//
+//}finally{
+//  3-cerramos la conexion
+//}
--- src/com/objetos/Persona.java
+++ src/com/objetos/Persona.java
@@ -2,8 +2,105 @@ package com.objetos;
 
 public class Persona {
 
-    public String nombre;
-    public String apellidos;
-    public int edad;
+    // las propiedades son privadas. 
+    private String nombre;
+    private String apellidos;
+    private int edad;
+    private int dni;
+
+    public enum tipoGenero{
+        FEMENINO, MASCULINO, OTRO
+    }
+
+    private tipoGenero genero;    
+
+    // por cada propiedad tenemos dos metodos: GET y SET:
+
+    // GET para recuperar ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡
+    public String getNombre(){
+        return nombre;
+    }
+    public String getApellidos(){
+        return apellidos;
+    }
+    public int getEdad(){
+        return edad;
+    }
+    public tipoGenero getGenero() {
+        return this.genero;
+    }
+    public int getDni() {
+        return this.dni;
+    }
+
+    // SET para establecer un valor ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public void setApellidos(String apellidos){
+        this.apellidos = apellidos;
+    }
+    public void setEdad(int edad) throws Exception {
+
+        // controlamos la edad
+        // 1.- error silencioso, 
+        //if (edad < 0) {
+        //    this.edad = 0;
+        //}else{
+        //    this.edad = edad;
+        //}
+
+        // controlamos la edad
+        // 2.- error grave, nosotros lo controlamos 
+        if (edad < 0) {
+            throw new Exception("La edad no puede ser negativa");
+        }else{
+            this.edad = edad;
+        }
+    }
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public void setGenero(tipoGenero genero) {
+            this.genero = genero;
+    }
+    // ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡
+
+    // nombre completo
+    public String getNombreCompleto(){
+        return this.nombre + " " + this.apellidos;
+    }
+    // en mayusculas
+    public String getNombreCompletoMayusculas(){
+        return this.nombre.toUpperCase() + " " + this.apellidos.toUpperCase();
+    }
+    // otro metodo para devolver el numbre completo pero alreves
+    public String getNombreCompletoAlreves(){
+        return this.apellidos + " " + this.nombre;
+    }
+    // metodo con nombre y apellidos con un separador
+    public String getNombreSeparador(String separador){
+        return this.nombre + " " + separador + " " + this.apellidos;
+    }
+
+    // ------------ calcula de la letra del dni -----------------
+    public char getLetraDni(){
+
+        String letraDni = "TRWAGMYFPDXBNJZSQVHLCKET";
     
+        int resultado = (dni - (dni / 23) * 23);
+        char letra = letraDni.charAt(resultado);
+        return letra;
+    
+    }
+
+    // si ponemos el minmo nombre del metodo a todos, los podemos diferencial por un nombre definido
+    // public String getNombreCompleto(int numero){
+    // }
+    // public String getNombreCompleto(String numero){
+    // }
+    // public String getNombreCompleto(boolean numero){
+    // }
+    // lo que importa es la firma de un metodo
 }
